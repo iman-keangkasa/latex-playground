9cbf41ce64efdc143d5d4119102f21fe88697e17
hafiz.asus <hafiz.ghazman@gmail.com>
2018-10-14 10:57:20 +0800

    Add autoremove latex installed from apt

---
+++ auto-remove-latex.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get purge texlive*
+sudo rm -rf /usr/local/texlive/*
+rm -rf ~/.texlive*
+sudo rm -rf /usr/local/share/texmf
+sudo rm -rf /var/lib/texmf
+sudo rm -rf /etc/texmf
+sudo apt-get remove tex-common --purge
+rm -rf ~/.tex*
+find -L /usr/local/bin -lname /usr/local/texlive/*/bin/* | xargs rm
